b9f208e77bcb654559d8ab9ad7d9ba0f9122c2d4
chai-bpt <chai.bpt@gmail.com>
2021-07-31 20:02:45 +0530

    semaphore used instade of custom lock mechanism

--- test.c
+++ test.c
@@ -3,13 +3,20 @@
 #include <pthread.h>
 #include <sched.h>
 #include <string.h>
+#include <semaphore.h>
 
 #define LOOP_COUNT_MAX 10000000
 
+/*
 typedef struct __lock_t {
         int iQueueTailTkt;
 	int iCurrentTkt;
 } lock_t;
+*/
+
+typedef struct __lock_t {
+	sem_t sem ;
+}lock_t;
 
 unsigned int uigGlobVar = 0;
 
@@ -17,24 +24,17 @@ lock_t lock_uigGlobVar;
 
 void lock_init(lock_t* plock)
 {
-	memset(plock, 0, sizeof(lock_t));
-	plock->iQueueTailTkt = 0;
-	plock->iCurrentTkt = 0;
+	sem_init(&plock->sem, 0, 1);
 }
 
 void lock_get(lock_t* plock)
 {
-	int iMyTkt = 0;
-
-	iMyTkt = __sync_fetch_and_add(&plock->iQueueTailTkt, 1);
-	while(iMyTkt != plock->iCurrentTkt);
-		sched_yield();
-	
+	sem_wait(&plock->sem);
 }
 
 void lock_put(lock_t* plock)
 {
-	plock->iCurrentTkt++;
+	sem_post(&plock->sem);
 }
 
 
